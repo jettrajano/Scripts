c08f83b537f6d9a67657561f47e3d4c27062d512
Tadeu Trajano <t2.tadeu@hotmail.com>
2023-12-03 14:02:21 -0300

    https://github.com/jettrajano/Scripts/tree/https/github.com/jettrajano/Scripts

---
+++ Scripts/Adicionar_usuario_Grupo_ADLocal.ps1
@@ -0,0 +1,5 @@
+﻿Import-Module ActiveDirectory 
+ForEach ($Item in (Get-Content C:\scripts\office365\grupos\aaattt.txt)){ 
+    Get-ADUser -Filter ‘Name -eq $Item’ | Add-ADPrincipalGroupMembership -MemberOf "UPN do usuário a ser adicoinado"
+
+}
